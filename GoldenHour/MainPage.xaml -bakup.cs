d714160546a607f034f4e9fd0657d879a9e56fc4
Dermot Boyle <dermotb@hotmail.com>
2013-09-23 15:58:08 +0100

    Initial addition to GIT

---
+++ GoldenHour/MainPage.xaml -bakup.cs
@@ -0,0 +1,189 @@
+﻿using Bing.Maps;
+using GoldenHour.Data;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Devices.Geolocation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Display;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+// The Items Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234233
+
+namespace GoldenHour
+{
+    /// <summary>
+    /// A page that displays a collection of item previews.  In the Split Application this page
+    /// is used to display and select one of the available groups.
+    /// </summary>
+    public sealed partial class MainPage : GoldenHour.Common.LayoutAwarePage
+    {
+		bool usingCurrentPosition = true;
+		Geolocator geolocator;
+		LocationIcon locationIcon;
+		//MapLayer mMapLayer;
+
+
+        public MainPage()
+        {
+			this.InitializeComponent();
+			geolocator = new Geolocator();
+			locationIcon = new LocationIcon();
+
+			// Add the location icon to map layer so that we can position it.
+			map.Children.Add(locationIcon);
+//			mMapLayer = new MapLayer();
+//			map.Children.Add(mMapLayer);
+
+			AddCurrentLocationData();
+
+//			geolocator.PositionChanged += new Windows.Foundation.TypedEventHandler<Geolocator, PositionChangedEventArgs>(geolocator_PositionChanged);
+
+        }
+
+		private async void AddCurrentLocationData()
+		{
+			Geoposition current = await geolocator.GetGeopositionAsync();
+			displayPosition(current.Coordinate.Latitude, current.Coordinate.Longitude, true);
+			accuracy.Text = current.Coordinate.Accuracy.ToString();
+		}
+
+		private void map_RightTapped_1(object sender, RightTappedRoutedEventArgs e)
+		{
+			usingCurrentPosition = false;
+			Location location = new Location();
+			bool succeeded = map.TryPixelToLocation(e.GetPosition(map), out location);
+
+			if (succeeded)
+			{
+//				Pushpin pin = new Pushpin();
+//				mMapLayer.Children.Add(pin);
+//				MapLayer.SetPosition(pin, location);
+				displayPosition(location.Latitude, location.Longitude, false);
+			}
+		}
+
+/*		private void geolocator_PositionChanged(Geolocator sender, PositionChangedEventArgs args)
+		{
+			try
+			{
+				if (usingCurrentPosition)
+				{
+					// Need to set map view on UI thread.
+					this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, new DispatchedHandler(
+						() =>
+						{
+							displayPosition(args.Position.Coordinate.Latitude, args.Position.Coordinate.Longitude);
+						}));
+
+					this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, new DispatchedHandler(() =>
+					{
+						accuracy.Text = args.Position.Coordinate.Accuracy.ToString();
+					}));
+
+				}
+			}
+			catch (Exception e)
+			{
+				string x = e.Message;
+				this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, new DispatchedHandler(() =>
+				{
+					status.Text = "An error has occurred processing position data";
+				}));
+			}
+
+		}*/
+
+		private void displayPosition(double lat, double lon, bool home)
+		{
+			Location location = new Location(lat, lon);
+			MapLayer.SetPosition(locationIcon, location);
+
+			if (home)
+			{
+				map.SetView(location, 15.0f);
+			}
+
+			DisplaySunData(location);
+	
+		}
+
+		private void DisplaySunData(Location location)
+		{
+			latText.Text = location.Latitude.ToString();
+			longText.Text = location.Longitude.ToString();
+
+			DateTime date = DateTime.Today;
+			bool isSunrise = false;
+			bool isSunset = false;
+			DateTime sunrise = DateTime.Now;
+			DateTime sunset = DateTime.Now;
+
+			SunTimes.LatitudeCoords stLatitude = DecimalLatToDegreesLat(location.Latitude);
+			SunTimes.LongitudeCoords stLongitude = DecimalLonToDegreesLon(location.Longitude);
+
+			SunTimes.Instance.CalculateSunRiseSetTimes(location.Latitude, location.Longitude, date, ref sunrise, ref sunset, ref isSunrise, ref isSunset);
+
+			sunRise.Text = sunrise.ToLocalTime().TimeOfDay.ToString();
+			sunSet.Text = sunset.ToLocalTime().TimeOfDay.ToString();
+		}
+
+		private SunTimes.LatitudeCoords DecimalLatToDegreesLat(double lat)
+		{
+			int sec = (int)Math.Round(lat * 3600);
+			int deg = sec / 3600;
+			sec = Math.Abs(sec % 3600);
+			int min = sec / 60;
+			sec %= 60;
+
+			SunTimes.LatitudeCoords stLat = new SunTimes.LatitudeCoords(deg, min, sec, (lat > 0)? SunTimes.LatitudeCoords.Direction.North: SunTimes.LatitudeCoords.Direction.South);
+			return stLat;
+		}
+
+		private SunTimes.LongitudeCoords DecimalLonToDegreesLon(double lon)
+		{
+			int sec = (int)Math.Round(lon * 3600);
+			int deg = sec / 3600;
+			sec = Math.Abs(sec % 3600);
+			int min = sec / 60;
+			sec %= 60;
+
+			SunTimes.LongitudeCoords stLat = new SunTimes.LongitudeCoords(deg, min, sec, (lon > 0)? SunTimes.LongitudeCoords.Direction.East : SunTimes.LongitudeCoords.Direction.West);
+			return stLat;
+		}
+
+
+		private void CurrentCalc_Click(object sender, RoutedEventArgs e)
+		{
+			AddCurrentLocationData();
+//			usingCurrentPosition = true;
+//			geolocator.GetGeopositionAsync();
+		}
+
+        /// <summary>
+        /// Populates the page with content passed during navigation.  Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="navigationParameter">The parameter value passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested.
+        /// </param>
+        /// <param name="pageState">A dictionary of state preserved by this page during an earlier
+        /// session.  This will be null the first time a page is visited.</param>
+        protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
+        {
+
+        }
+
+	}
+}
---
+++ GoldenHour/WpfUtilities.cs
@@ -0,0 +1,63 @@
+﻿using GoldenHour;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace GoldenHour.Common
+{
+
+    public static class WpfUtilities
+    {
+        internal static T FindResource<T>(this FrameworkElement e, string name)
+        {
+            object value = null;
+            DependencyObject d = e;
+            while (d != null)
+            {
+                FrameworkElement f = d as FrameworkElement;
+                if (f != null)
+                {
+                    if (f.Resources.TryGetValue(name, out value))
+                    {
+                        return (T)value;
+                    }
+                }
+                d = VisualTreeHelper.GetParent(d);
+            }
+
+            if (App.Current.Resources.TryGetValue(name, out value))
+            {
+                return (T)value;
+            }
+
+            return default(T);
+        }
+    }
+
+    public class ShortDateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            DateTime date = (DateTime)value;
+            return date.ToString("dd MMMM yyyy");
+			//return date.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            string s = (string)value;
+            DateTime d;
+            if (DateTime.TryParse(s, out d))
+            {
+                return d;
+            }
+            return DateTime.Today;
+        }
+    }
+}
